9740145bcebcba9d2276d723acf219cefb47228c
KanTheCodingNoob <s4019548@rmit.edu.vn>
2025-06-19 02:06:28 +0700

    Create a folder in data when first open the site

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::path::PathBuf;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,7 +10,20 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
+        .setup(|_app| {
+            if let Some(appdata_path) = dirs::data_dir() {
+                let folder_path: PathBuf = appdata_path.join("thao_management");
+                match fs::create_dir_all(&folder_path) {
+                    Ok(_) => println!("Created folder at: {}", folder_path.display()),
+                    Err(e) => eprintln!("Failed to create folder: {}", e),
+                }
+            } else {
+                eprintln!("Could not locate AppData.");
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
