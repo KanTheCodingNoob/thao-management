5eff1fa5bcd97abfcc0b9507a9755f7ba6423013
KanTheCodingNoob <s4019548@rmit.edu.vn>
2025-06-19 17:15:36 +0700

    Homepage

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,45 @@
 use std::fs;
 use std::path::PathBuf;
+use once_cell::sync::OnceCell;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+static APP_FOLDER_PATH: OnceCell<PathBuf> = OnceCell::new();
+
+// Initialise a folder in Appdata
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn init_app_folder() -> Result<String, String> {
+    if let Some(appdata_path) = dirs::data_dir() {
+        let folder_path: PathBuf = appdata_path.join("thao_management");
+        match fs::create_dir_all(&folder_path) {
+            Ok(_) => {
+                APP_FOLDER_PATH.set(folder_path.clone()).ok();
+                Ok(format!("Created folder at: {}", folder_path.display()))
+            },
+            Err(e) => Err(format!("Failed to create folder: {}", e)),
+        }
+    } else {
+        Err("Could not locate AppData directory.".to_string())
+    }
+}
+
+// Check if the directory is empty
+#[tauri::command]
+fn is_directory_empty() -> Result<bool, String> {
+    match APP_FOLDER_PATH.get() {
+        Some(path) => match fs::read_dir(path) {
+            Ok(mut entries) => Ok(entries.next().is_none()),
+            Err(err) => Err(format!("Failed to read directory: {}", err)),
+        },
+        None => Err("App folder is not initialized.".to_string())
+    }
+}
+
+// Allow the frontend to access the path to data
+#[tauri::command]
+fn get_app_folder_path_str() -> Result<String, String> {
+    match APP_FOLDER_PATH.get() {
+        Some(path) => Ok(path.display().to_string()),
+        None => Err("App folder is not initialized.".to_string()),
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -12,19 +47,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .setup(|_app| {
-            if let Some(appdata_path) = dirs::data_dir() {
-                let folder_path: PathBuf = appdata_path.join("thao_management");
-                match fs::create_dir_all(&folder_path) {
-                    Ok(_) => println!("Created folder at: {}", folder_path.display()),
-                    Err(e) => eprintln!("Failed to create folder: {}", e),
-                }
-            } else {
-                eprintln!("Could not locate AppData.");
-            }
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![init_app_folder, is_directory_empty, get_app_folder_path_str])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
